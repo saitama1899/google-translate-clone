cf2185bedea72ce695bc6e1dfe99f1ceba68599b
saitama1899 <eric.lorien92@gmail.com>
2023-03-30 09:51:52 +0200

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App () {
+  return (
+    <div className="App">
+     <h1 className='text-xl'>Google translate</h1>
+    </div>
+  )
+}
+
+export default App
