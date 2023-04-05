e11d7fba96ae6ae9fc06d13e6a289e2b2bd98997
saitama1899 <eric.lorien92@gmail.com>
2023-04-05 10:11:34 +0200

    add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {}
   },
