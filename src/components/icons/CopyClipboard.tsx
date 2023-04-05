86f5ae6fb8ef52a846629b97764850b7e177b2b9
saitama1899 <eric.lorien92@gmail.com>
2023-04-05 10:14:28 +0200

    add icons

---
+++ src/components/icons/CopyClipboard.tsx
@@ -0,0 +1,16 @@
+const CopyClipboardIcon = (props: any) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path fill="none" d="M0 0H24V24H0z"></path>
+      <path d="M16 20H5V6H3v14c0 1.1.9 2 2 2h11v-2zm4-4V4c0-1.1-.9-2-2-2H9c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2zm-2 0H9V4h9v12z"></path>
+    </svg>
+  )
+}
+
+export default CopyClipboardIcon
